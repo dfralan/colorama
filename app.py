ae5bc015b1029f427299268e26fb5b1c40eabf9a
dfralan <80047722+dfralan@users.noreply.github.com>
2021-08-11 15:50:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ hex_full = '#' + hex_number
 print('Eeeexito, look for me for',hex_full)
   
 str(hex_number)
-f = open(str(hex_full)+'.svg', 'x')
+f = open(str(hex_full)+'.svg', 'a')
 f.write('<?xml version="1.0" encoding="UTF-8"?><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="500px" height="500px" viewBox="0 0 700 700" version="1.1"><g><rect x="0" y="0" width="700" height="700" style="fill:#' + str(hex_number) + ';"/></g></svg>')
 f.close()
