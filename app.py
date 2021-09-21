9bea6780094bd450d5501d163cdf9e9a3bdd2ce2
dfralan <80047722+dfralan@users.noreply.github.com>
2021-09-21 15:26:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ def genColorama():
         hex_number = format(random_number,'x')
         hex_full = '#' + hex_number + '.svg'
 
-        if hex_full in os.listdir("Hexas"):
+        if hex_full in os.listdir("Colors"):
             print("El " + hex_full + " estaba repetido rey, we re alcahuete, sigooooooooooooooooooooooooooooo")
             return genColorama()        
         
         else:
-            file = open('Hexas/'+hex_full, 'a')
+            file = open('Colors/'+hex_full, 'a')
             file.write('<?xml version="1.0" encoding="UTF-8"?><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="500px" height="500px" viewBox="0 0 500 500" version="1.1"><g><rect x="0" y="0" width="500" height="500" style="fill:#' + str(hex_number) + ';"/></g></svg>')
             print('Color generado, se llama ',hex_full)
             file.close()
