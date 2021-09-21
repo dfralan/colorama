b6b9b09a7c7e21a08d0c10d38421123c9e305f54
dfralan <80047722+dfralan@users.noreply.github.com>
2021-09-21 17:33:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
 import random
+from selenium import webdriver
 import os
 
 
 
 def genColorama():
     while True:
-        random_number = random.randint(0,16777215)
-        hex_number = format(random_number,'x')
-        hex_full = '#' + hex_number + '.svg'
-
-        if hex_full in os.listdir("Colors"):
-            print("El " + hex_full + " estaba repetido rey, we re alcahuete, sigooooooooooooooooooooooooooooo")
-            return genColorama()        
-        
+        color = '#' + "%06x" % random.randint(0, 0xFFFFFF)
+        color_file = color + '.svg'
+        if color_file in os.listdir("Colors"):
+            print("El " + color + " estaba repetido rey, sigooooooooooooooooooooooooooooo!")
+            return genColorama()
         else:
-            file = open('Colors/'+hex_full, 'a')
-            file.write('<?xml version="1.0" encoding="UTF-8"?><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="500px" height="500px" viewBox="0 0 500 500" version="1.1"><g><rect x="0" y="0" width="500" height="500" style="fill:#' + str(hex_number) + ';"/></g></svg>')
-            print('Color generado, se llama ',hex_full)
+            file = open('Colors/' + color_file, 'a')
+            file.write('<?xml version="1.0" encoding="UTF-8"?><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="500px" height="500px" viewBox="0 0 500 500" version="1.1"><g><rect x="0" y="0" width="500" height="500" style="fill:' + color + ';"/></g></svg>')
+            print('El color generado se llama ' + color)
             file.close()
-
 genColorama()
