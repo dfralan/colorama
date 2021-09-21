467de30f5fc8d92fb1335295209eac8abf338be5
dfralan <80047722+dfralan@users.noreply.github.com>
2021-09-21 15:22:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import random
+import os
 
-random_number = random.randint(0,16777215)
-hex_number =format(random_number,'x')
-hex_full = '#' + hex_number
-print('Eeeexito, look for me for as',hex_full)
-  
-str(hex_number)
-f = open(str(hex_full)+'.svg', 'a')
-f.write('<?xml version="1.0" encoding="UTF-8"?><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="500px" height="500px" viewBox="0 0 700 700" version="1.1"><g><rect x="0" y="0" width="700" height="700" style="fill:#' + str(hex_number) + ';"/></g></svg>')
-f.close()
+
+
+def genColorama():
+    while True:
+        random_number = random.randint(0,16777215)
+        hex_number = format(random_number,'x')
+        hex_full = '#' + hex_number + '.svg'
+
+        if hex_full in os.listdir("Hexas"):
+            print("El " + hex_full + " estaba repetido rey, we re alcahuete, sigooooooooooooooooooooooooooooo")
+            return genColorama()        
+        
+        else:
+            file = open('Hexas/'+hex_full, 'a')
+            file.write('<?xml version="1.0" encoding="UTF-8"?><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="500px" height="500px" viewBox="0 0 500 500" version="1.1"><g><rect x="0" y="0" width="500" height="500" style="fill:#' + str(hex_number) + ';"/></g></svg>')
+            print('Color generado, se llama ',hex_full)
+            file.close()
+
+genColorama()
